bb521f0115957ec290f60ec4093da311b37ab78c
Oksana Shcherban <oksana.shcherban@bonial.com>
2021-10-26 15:39:34 +0200

    added ARCL library, switching between SceneLocationView and ARView, adding an annotation for far objects

--- Hunter/ContentView.swift
+++ Hunter/ContentView.swift
@@ -7,31 +7,80 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+import CoreLocation
+import ARCL
 
-struct ContentView : View {
+struct ContentView: View {
+    
+    @State private var showRealityView = false
+    @State private var boxesCollected = 0
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        VStack {
+            if showRealityView {
+                ARViewContainer()
+            }
+            if !showRealityView {
+                SceneViewContainer()
+            }
+                //.edgesIgnoringSafeArea(.all)
+            Button(action: {
+                showRealityView.toggle()
+            }) {
+                Text("Switch")
+            }
+//            Text("You've collected \(boxesCollected) boxes! ")
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
+        
     func makeUIView(context: Context) -> ARView {
-        
+
         let arView = ARView(frame: .zero)
-        
+
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        let box = try! Experience.loadBox() // type: Entity
+        arView.scene.anchors.append(box)
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        print()
+    }
+}
+
+struct SceneViewContainer: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> SceneLocationView {
+
+        let sceneLocationView = SceneLocationView()
+        sceneLocationView.run()
+        
+        let locationManager = CLLocationManager()
+        let current: CLLocation? = locationManager.location
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 52.51364861314343, longitude: 13.450571390720818)
+        let location = CLLocation(coordinate: coordinate, altitude: CLLocationDistance(current?.altitude ?? 0))
+        
+
+        let squareView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        squareView.backgroundColor = .red
+
+        let annotationNode = LocationAnnotationNode(location: location, view: squareView)
+        annotationNode.scaleRelativeToDistance = true
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        
+        return sceneLocationView
+    }
+
+    func updateUIView(_ uiView: SceneLocationView, context: Context) {
+        print()
+    }
+
 }
 
 #if DEBUG
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+target 'Hunter' do
+  use_frameworks!
+
+  # Pods for Hunter
+  pod 'ARCL'
+end
