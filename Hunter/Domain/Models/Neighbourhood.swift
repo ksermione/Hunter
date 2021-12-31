b32996823f99b2e8e99a6db5e25b433a59cebb18
Oksana Shcherban <oksana.shcherban@bonial.com>
2021-12-31 13:38:58 +0100

    added near and far locations

--- Hunter/Domain/Models/Neighbourhood.swift
+++ Hunter/Domain/Models/Neighbourhood.swift
@@ -9,13 +9,15 @@ import Foundation
 import CoreLocation
 
 enum Neighbourhood: String {
-    case friedrichshain = "Friedrichshain"
+    case friedrichshainNear = "Around Frankfurter Tor"
+    case friedrichshainFar = "Friedrichshain"
+    case friedrichshainTest = "Kadiner Test"
     case pBerg = "Prenzlauerberg"
     case mitte = "Mitte"
     
     var locations: [CLLocation] {
         switch self {
-        case .friedrichshain:
+        case .friedrichshainFar:
             return [
                 // all of fhain - big walk locations
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51087, longitude: 13.45985), altitude: CLLocationDistance(40)), // boxi
@@ -25,9 +27,19 @@ enum Neighbourhood: String {
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51880, longitude: 13.44480), altitude: CLLocationDistance(42)), // auerdreieck
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.52131, longitude: 13.45055), altitude: CLLocationDistance(54)), // petersburger platz
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51944, longitude: 13.46035), altitude: CLLocationDistance(51)), // Fockenbeckplatz
-                
-                
-                
+            ]
+        case .friedrichshainNear:
+            return [
+                // locations near to FR tor
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51532, longitude: 13.45289), altitude: CLLocationDistance(47)),
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51328, longitude: 13.45292), altitude: CLLocationDistance(41)),
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51445, longitude: 13.45467), altitude: CLLocationDistance(42)),
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51589, longitude: 13.45714), altitude: CLLocationDistance(45)),
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51639, longitude: 13.45222), altitude: CLLocationDistance(48)),
+                CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.51543, longitude: 13.45588), altitude: CLLocationDistance(43)),
+            ]
+        case .friedrichshainTest:
+            return [
                 // local ones for testing near Kadiner
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.511687, longitude: 13.447388), altitude: CLLocationDistance(40)),
                 CLLocation(coordinate: CLLocationCoordinate2D(latitude: 52.512910, longitude: 13.450392), altitude: CLLocationDistance(40)),
